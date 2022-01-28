4c9ed32ba5ae1b01c2146f09d1d277c39eda4d10
Giancarlo Pernudi Segura <pernudi@ualberta.ca>
2022-01-28 02:53:04 -0700

    add optional gpg steps

--- gpg.sh
+++ gpg.sh
@@ -11,3 +11,13 @@ gpg --list-secret-keys --keyid-format=long
 gpg --armor --export YOUR_GPG_KEY_ID
 # Add the gpg key to your account here:
 # https://github.com/settings/gpg/new
+
+# Optional steps
+
+# Add the gpg key to your git for automatic signing
+git config --global user.signingkey YOUR_GPG_KEY_ID
+git config --global gpg.program $(which gpg)
+git config --global commit.gpgsign true
+
+# Add the following line to your /.bash_profile, ~/.bashrc, or ~/.zshrc file
+export GPG_TTY=$(tty)
